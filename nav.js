e45ac24679f6c1b56f55d18f7ce7c0696694fe1b
aditya singh <mail2adityasinghsln@gmail.com>
2022-02-28 00:50:52 +0530

    update

--- nav.js
+++ nav.js
@@ -6,4 +6,10 @@ homeBtn.addEventListener("click", function(){
     window.location.assign("./index.html");
 })
 
+githubBtn.addEventListener("click", function(){
+    window.location.assign("https://github.com/git-aditya-singh/sudoku-main");
+})
 
+linkedinBtn.addEventListener("click", function(){
+    window.location.assign("https://www.linkedin.com/in/aditya-pratap-singh-68227a1bb/");
+})
