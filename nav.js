25ae7915322c9d7458568279fd808e991a403039
aditya singh <mail2adityasinghsln@gmail.com>
2022-02-28 00:49:28 +0530

    sudoku

---
+++ nav.js
@@ -0,0 +1,9 @@
+let homeBtn = document.querySelector("#home");
+let githubBtn = document.querySelector("#github");
+let linkedinBtn = document.querySelector("#linkedin");
+
+homeBtn.addEventListener("click", function(){
+    window.location.assign("./index.html");
+})
+
+
